ff540ac26480eeb863693e36e5be6568695eb3d4
aimtiaz <aimtiaz3@uh.edu>
2016-11-04 00:15:30 -0500

    dropdown

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+north_american = State.create(state: 'North America')
